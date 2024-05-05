7abe158bf2f87523349b4f794f588c1c14b62402
GeorgeT7 <george.pop@student.upt.ro>
2024-05-05 21:39:53 +0300

    added setup of relay pin

--- arduino/ArduinoMega/Arduino_Mega/relay_functions.cpp
+++ arduino/ArduinoMega/Arduino_Mega/relay_functions.cpp
@@ -1,5 +1,9 @@
 #include "relay_functions.h"
 
+void setupRelay(uint8_t pin) {
+  pinMode(pin, OUTPUT);
+}
+
 void turnOnRelay(uint8_t relay) {
   digitalWrite(relay, LOW);
 }
--- arduino/ArduinoMega/Arduino_Mega/relay_functions.h
+++ arduino/ArduinoMega/Arduino_Mega/relay_functions.h
@@ -2,6 +2,7 @@
 #define RELAY_FUNCTIONS_H
 #include <Arduino.h>
 
+void setupRelay(uint8_t pin);
 void turnOnRelay(uint8_t relay);
 void turnOffRelay(uint8_t relay);
 
