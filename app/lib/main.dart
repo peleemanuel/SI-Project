f1b7cb5fc1615ce4a47b3f884f57e1aa3e50f673
paula <paula.pistrila@student.upt.ro>
2024-05-22 23:33:13 +0300

    app frontend

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,125 +1,168 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: HomeScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class HomeScreen extends StatelessWidget {
+  final List<Map<String, dynamic>> plantData = [
+    {
+      'plant': 'Plant 1',
+      'humidity': 55,
+      'temperature': 22,
+      'light': 1500,
+      'soilMoisture': 35,
+    },
+    {
+      'plant': 'Plant 2',
+      'humidity': 60,
+      'temperature': 23,
+      'light': 1600,
+      'soilMoisture': 33,
+    },
+    {
+      'plant': 'Plant 3',
+      'humidity': 58,
+      'temperature': 21,
+      'light': 1550,
+      'soilMoisture': 34,
+    },
+    {
+      'plant': 'Plant 4',
+      'humidity': 57,
+      'temperature': 24,
+      'light': 1580,
+      'soilMoisture': 36,
+    },
+  ];
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Smart Plant Greenhouse'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: ListView.builder(
+        itemCount: plantData.length,
+        itemBuilder: (context, index) {
+          final plant = plantData[index];
+          return Card(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(plant['plant'], style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+                  Text('Humidity: ${plant['humidity']}%'),
+                  Text('Temperature: ${plant['temperature']}°C'),
+                  Text('Light: ${plant['light']} lx'),
+                  Text('Soil Moisture: ${plant['soilMoisture']}%'),
+                ],
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          );
+        },
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () {
+              // Logic for opening hatch
+            },
+            child: Icon(Icons.open_in_new),
+            tooltip: 'Open Hatch',
+          ),
+          SizedBox(height: 16),
+          FloatingActionButton(
+            onPressed: () {
+              // Logic for turning light on/off
+            },
+            child: Icon(Icons.lightbulb),
+            tooltip: 'Turn Light On/Off',
+          ),
+          SizedBox(height: 16),
+          FloatingActionButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => AddDataScreen()),
+              );
+            },
+            child: Icon(Icons.add),
+            tooltip: 'Add Data',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class AddDataScreen extends StatelessWidget {
+  final TextEditingController tempMinController = TextEditingController();
+  final TextEditingController tempMaxController = TextEditingController();
+  final TextEditingController humiditySoilMinController = TextEditingController();
+  final TextEditingController humiditySoilMaxController = TextEditingController();
+  final TextEditingController humidityAirMinController = TextEditingController();
+  final TextEditingController humidityAirMaxController = TextEditingController();
+  final TextEditingController lightController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Add Data'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: tempMinController,
+              decoration: InputDecoration(labelText: 'Min Temperature'),
+            ),
+            TextField(
+              controller: tempMaxController,
+              decoration: InputDecoration(labelText: 'Max Temperature'),
+            ),
+            TextField(
+              controller: humiditySoilMinController,
+              decoration: InputDecoration(labelText: 'Min Soil Humidity'),
+            ),
+            TextField(
+              controller: humiditySoilMaxController,
+              decoration: InputDecoration(labelText: 'Max Soil Humidity'),
+            ),
+            TextField(
+              controller: humidityAirMinController,
+              decoration: InputDecoration(labelText: 'Min Air Humidity'),
+            ),
+            TextField(
+              controller: humidityAirMaxController,
+              decoration: InputDecoration(labelText: 'Max Air Humidity'),
+            ),
+            TextField(
+              controller: lightController,
+              decoration: InputDecoration(labelText: 'Light (lx)'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // Code to save data
+              },
+              child: Text('Save Data'),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
